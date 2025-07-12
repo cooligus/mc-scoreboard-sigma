8fdee571fdb7eb52e8827421db5a068e17a775ee
cooligus <pewienjaaa@gmail.com>
2025-07-12 12:24:43 +0200

    Up

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	base: import.meta.env.BASE_PATH || '/',
+	base: process.env.BASE_PATH || '/',
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
